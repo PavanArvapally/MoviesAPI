2299a5d05dcf286fb5f8d9faa87275180aff26dc
PavanArvapally <arvapallypavan@gmail.com>
2023-10-04 06:45:36 +0530

    Update MoviesApplication.java

--- src/main/java/com/pavan/movies/MoviesApplication.java
+++ src/main/java/com/pavan/movies/MoviesApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.lang.*;
 
 @SpringBootApplication
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+		System.out.println("Cherry commit");
 	}
 
 
